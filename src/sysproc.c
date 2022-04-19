2abd932d67a28a191f4420daee44cd9eba2c4095
rajdashora <raj.dashora1@gmail.com>
2022-04-18 19:13:51 -0500

    moved into src directory

--- src/sysproc.c
+++ src/sysproc.c
@@ -7,67 +7,62 @@
 #include "mmu.h"
 #include "proc.h"
 
-int
-sys_fork(void)
+int sys_fork(void)
 {
   return fork();
 }
 
-int
-sys_exit(void)
+int sys_exit(void)
 {
   exit();
-  return 0;  // not reached
+  return 0; // not reached
 }
 
-int
-sys_wait(void)
+int sys_wait(void)
 {
   return wait();
 }
 
-int
-sys_kill(void)
+int sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
 
-int
-sys_getpid(void)
+int sys_getpid(void)
 {
   return myproc()->pid;
 }
 
-int
-sys_sbrk(void)
+int sys_sbrk(void)
 {
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
 
-int
-sys_sleep(void)
+int sys_sleep(void)
 {
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -79,8 +74,7 @@ sys_sleep(void)
 
 // return how many clock tick interrupts have occurred
 // since start.
-int
-sys_uptime(void)
+int sys_uptime(void)
 {
   uint xticks;
 
@@ -90,44 +84,50 @@ sys_uptime(void)
   return xticks;
 }
 
-//changed: added wrapper here
-int sys_mencrypt(void) {
+// changed: added wrapper here
+int sys_mencrypt(void)
+{
   int len;
-  char * virtual_addr;
+  char *virtual_addr;
 
-  if(argint(1, &len) < 0)
+  if (argint(1, &len) < 0)
     return -1;
-  if (len <= 0) {
+  if (len <= 0)
+  {
     return -1;
   }
-  if(argptr(0, &virtual_addr, 1) < 0)
+  if (argptr(0, &virtual_addr, 1) < 0)
     return -1;
-  if ((void *) virtual_addr >= P2V(PHYSTOP)) {
+  if ((void *)virtual_addr >= P2V(PHYSTOP))
+  {
     return -1;
   }
   return mencrypt(virtual_addr, len);
 }
 
-int sys_getpgtable(void) {
-  struct pt_entry * entries; 
+int sys_getpgtable(void)
+{
+  struct pt_entry *entries;
   int num;
 
-  if(argint(1, &num) < 0)
+  if (argint(1, &num) < 0)
     return -1;
-  if(argptr(0, (char**)&entries, num*sizeof(struct pt_entry)) < 0){
+  if (argptr(0, (char **)&entries, num * sizeof(struct pt_entry)) < 0)
+  {
     return -1;
   }
-  return getpgtable(entries, num);
+
+  return getpgtable(entries, num, wsetOnly);
 }
 
+int sys_dump_rawphymem(void)
+{
+  char *physical_addr;
+  char *buffer;
 
-int sys_dump_rawphymem(void) {
-  char * physical_addr; 
-  char * buffer;
-
-  if(argptr(1, &buffer, PGSIZE) < 0)
+  if (argptr(1, &buffer, PGSIZE) < 0)
     return -1;
-  if(argint(0, (int*)&physical_addr) < 0)
+  if (argint(0, (int *)&physical_addr) < 0)
     return -1;
   return dump_rawphymem(physical_addr, buffer);
 }
